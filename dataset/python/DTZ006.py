eaa6947a6cbc91735be07f3b675ec4b495ac0829
yahya jabary <jabaryyahya@gmail.com>
2023-11-16 00:20:25 -0500

    update

---
+++ dataset/python/DTZ006.py
@@ -0,0 +1,21 @@
+import datetime
+
+# no args
+datetime.datetime.fromtimestamp(1234)
+
+# wrong keywords
+datetime.datetime.fromtimestamp(1234, bad=datetime.timezone.utc)
+
+# none args
+datetime.datetime.fromtimestamp(1234, None)
+
+# none keywords
+datetime.datetime.fromtimestamp(1234, tz=None)
+
+from datetime import datetime
+
+# no args unqualified
+datetime.fromtimestamp(1234)
+
+# uses `astimezone` method
+datetime.fromtimestamp(1234).astimezone()
