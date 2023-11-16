b39f13486bd8aeeb877f5701338130e382b34356
yahya jabary <jabaryyahya@gmail.com>
2023-11-16 00:17:37 -0500

    update

---
+++ dataset/python/0011_delete-url.py
@@ -0,0 +1,14 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("projects", "0010_migrate_domain_data"),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name="domain",
+            name="url",
+        ),
+    ]
