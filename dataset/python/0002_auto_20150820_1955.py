884cf41ead3371ade68fcdc0b4bfaa0a203d52f2
yahya jabary <jabaryyahya@gmail.com>
2023-11-16 00:16:22 -0500

    update

---
+++ dataset/python/0002_auto_20150820_1955.py
@@ -0,0 +1,9 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [("order", "0001_initial")]
+
+    operations = [
+        migrations.AlterModelOptions(name="payment", options={"ordering": ("-pk",)})
+    ]
