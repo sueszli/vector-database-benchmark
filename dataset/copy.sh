66ef3f63019aeedeeadfca243b7fffe07293efc6
yahya jabary <jabaryyahya@gmail.com>
2023-11-16 00:39:20 -0500

    update

--- dataset/copy.sh
+++ dataset/copy.sh
@@ -7,7 +7,7 @@ while [ "$(ls -A $src_dir)" ]; do
     cd $src_dir
 
     # we can't upload everything at once, because of github's commit size limit
-    for file in $(ls | head -1)
+    for file in $(ls | head -5000)
     do
         mv "$file" "$target_dir"
     done
